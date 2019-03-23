5c93a2a5d762acac3236aefcb88ed3e7fe7a5a3e
madbbb <madbbb@icloud.com>
2019-03-23 10:34:02 +0200

    Initial

--- server.js
+++ server.js
@@ -24,7 +24,19 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  let date;
+  if(req.params.date_string) {
+    date = new Date(req.params.date_string);
+  } else {
+    date = new Date();
+  }
+  
+  if(date == "Invalid Date") {
+    return res.json({"error" : "Invalid Date"});
+  }
+  res.json({ "unix": date.getTime(), "utc" : date.toUTCString() });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
